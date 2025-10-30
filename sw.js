c652a8743f7e3535de8b42060b0ebec6cbeae68f
mnaseralib-lgtm <mnaseralib@gmail.com>
2025-10-30 11:01:43 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('attendance-cache').then(cache => {
+      return cache.addAll(['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png']);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
